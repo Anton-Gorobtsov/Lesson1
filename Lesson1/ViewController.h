5418d2c9a9da21f9079cd10d53263c9d7b67e12e
Anton-Gorobtsov <a.gorobtsov@rambler.ru>
2015-04-28 14:55:09 +0300

    ДЗ1 obj-c 2

---
+++ Lesson1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Lesson1
+//
+//  Created by Triangle Development on 28.04.15.
+//  Copyright (c) 2015 Triangle Development. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Lesson1/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Lesson1
+//
+//  Created by Triangle Development on 28.04.15.
+//  Copyright (c) 2015 Triangle Development. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
